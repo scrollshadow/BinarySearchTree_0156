dbe937b5e8c3e0da70d3d5f1e356860f1f01d387
scrollshadow <zaldhypramana@gmail.com>
2026-06-12 15:40:49 +0700

    case 1

--- TREE.cpp
+++ TREE.cpp
@@ -138,4 +138,20 @@ int main()
         cout << "5. exit" << endl;
         cout << "\nEnter your choice (1-5): ";
 
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+
         
