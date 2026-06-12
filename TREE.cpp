457ea82b708304a2835dc62150966e6e0d02ad29
scrollshadow <zaldhypramana@gmail.com>
2026-06-12 15:18:22 +0700

    insert

--- TREE.cpp
+++ TREE.cpp
@@ -17,3 +17,27 @@ class node {
         rightChild = r;
     }
 };
+
+class BinaryTree {
+    //akses modif dan deklasrasi
+    public :
+    node* ROOT;
+    //membuat construktor ddan memberikan nilai paada objek root
+    BinaryTree() {
+        ROOT = nullptr;
+    }
+
+    //membuat prosedur instert
+    void insert(string element) {
+        //input nilai pada setiap objek
+        node* newNode = new node(element, nullptr, nullptr);
+        newNode->info = element;
+        newNode->leftChild = nullptr;
+        newNode->rightChild = nullptr;
+        
+        node* parent = nullptr;
+        node* currentNode = nullptr;
+        search(element, parent, currentNode);
+        
+    }
+};
