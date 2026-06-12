462c7df963afaae4de0f1db46aa7474af62ec2bd
scrollshadow <zaldhypramana@gmail.com>
2026-06-12 15:06:48 +0700

    class node dan variable

---
+++ TREE.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//class node
+class node {
+    //akses modifikasi dan deklarasi varable dan objeck node
+    public :
+    string info;
+    node* leftChild;
+    node* rightChild;
+};
